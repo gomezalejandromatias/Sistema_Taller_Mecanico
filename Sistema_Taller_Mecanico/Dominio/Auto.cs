62cd218837a31776326ebad7d2e923dfa7cecf90
gomezalejandromatias <mati.gorriti1@gmail.com>
2025-12-18 15:21:44 -0300

    Dominio/Clases

---
+++ Sistema_Taller_Mecanico/Dominio/Auto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio
+{
+    public class Auto
+    {
+
+        public int IdAuto { get; set; }
+        public string Patente { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Anio { get; set; }
+
+        public int IdCliente { get; set; }
+
+        // Composición: el auto "tiene" un motor
+        public Motor Motor { get; set; } = new Motor();
+
+        // Historial de reparaciones del auto
+        public List<Reparacion> Reparaciones { get; set; } = new List<Reparacion>();
+
+
+    }
+}
---
+++ Sistema_Taller_Mecanico/Sistema_Taller_Mecanico/Form1.cs
@@ -0,0 +1,10 @@
+namespace Sistema_Taller_Mecanico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
